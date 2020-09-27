fe7b0263ec93f7a1a62ff2c4647e3abfd332962e
marcatao <thiagomarcato@gmail.com>
2020-09-27 11:06:17 -0300

    Finalizando Comentarios

--- 01 - Fundamentos/Instalura/src/Components/Comentarios/Comentarios.js
+++ 01 - Fundamentos/Instalura/src/Components/Comentarios/Comentarios.js
@@ -1,18 +1,46 @@
-import React from 'react';
-import {FlatList, Text} from 'react-native'
+import React, { useState } from 'react';
+import {FlatList, Text, TextInput, View, Image, TouchableOpacity} from 'react-native'
 
 
 import style from './style';
 
 const Comentarios = ({comentarios}) => {
+    const [estComentario, setEstComentario] = useState(comentarios);
+    const adicionaComentario = () =>{
+        console.warn(conteudoCampoInput);
+        campoInput.clear();
+
+        const novoComentario = {
+            date: Date.now(),
+            text: conteudoCampoInput,
+            userName: "pauloscalercioalura"
+        }
+        setEstComentario([...estComentario, novoComentario]);
+        
+    }
+    let campoInput;
+    let conteudoCampoInput = '';
+
     return (
+    <View>
      <FlatList 
-        data={comentarios}
+        data={estComentario}
         keyExtractor={(item, index) => index.toString()}
            renderItem={({ item })=>
           <Text style={style.itemText}>{item.text}</Text>
          }
      />
+        <View style={style.comentsContainer}>
+            <TextInput 
+               ref={textInput => campoInput = textInput}
+               onChangeText={texto => conteudoCampoInput=texto }
+               placeholder={"Comentar..."} style={style.inputComentario}/>
+            <TouchableOpacity onPress={adicionaComentario}>
+                <Image source={require("../../../res/img/send.png")} style={style.imgSend} />
+            </TouchableOpacity>
+        </View>
+    </View>
+
     );
 }
 
--- 01 - Fundamentos/Instalura/src/Components/Comentarios/style.js
+++ 01 - Fundamentos/Instalura/src/Components/Comentarios/style.js
@@ -2,10 +2,32 @@ import {StyleSheet} from 'react-native'
 
 const style = StyleSheet.create({
     itemText:{
-        marginLeft:60,
+        marginLeft:25,
         padding:5,
         color: '#B1AFAF'
+    },
+    comentsContainer:{
+        flexDirection:"row",
+        padding:5,
+        alignItems:"center",
+    },
+    inputComentario:{
+        marginLeft:25,
+        flex:1,
+        borderBottomWidth : 1.0,
+        borderBottomColor:'#6A6A6A',
+        padding:5,
+        paddingBottom:1,
+    },
+    imgSend:{
+        width:25,
+        height:25,
+        marginRight:25,
+        marginLeft:10,
+        marginBottom:-10,
+       
     }
+    
 })
 
 export default style;
